428f499af14e184f6e78b5f138ec342959462e3f
eliza-alchinovaa-ui <eliza-alchinovaa@mail.ru>
2025-09-22 21:11:55 +0300

    Add files via upload

--- task2.cpp
+++ task2.cpp
@@ -1,19 +1,26 @@
-
+//#define _USE_MATH_DEFINES
 //#include <iostream>
+//#include <cmath>
 //using namespace std;
-//
-//int main()
-//{
-//    setlocale(LC_ALL, "rus");
-//
-//    int num;
-//    cout << "Введите число: ";
-//    cin >> num;
-//
-//    /*if (num > 0) cout << "Положительное число";
-//    else if (num = 0) cout << "Число равно нулю";
-//    else cout << "Отрицательное чило";*/
-//    /*if (num % 2 == 0) cout << "Чётное число";
-//    else cout << "Нечётное число";*/
-//    return 0;
+//int main() {
+//	setlocale(0, "");
+//	double x;
+//    
+//	cout << "Введите значение х: " << endl; cin >> x;
+//    double n = M_PI * abs(x);
+//    if (x >= -2.5 && x <= 2.5) {
+//        cout << "r = " << (5.0 / 2.0) * x * x * x * x;
+//    }
+//    else if (x > 2.5) {
+//        cout << "r = " << sqrt(2.0 * x + 1.0);
+//    }
+//    else {
+//        if (x == 3.0) {
+//            cout << "Ошибка: деление на ноль при x = 3.0";
+//        }
+//        else {
+//            cout << "r = " << (sin (n) ) / (x - 3.0);
+//        }
+//    }
+//	return 0;
 //}
--- task4.cpp
+++ task4.cpp
@@ -1,13 +1,52 @@
 //#include <iostream>
+//#include <string>
 //using namespace std;
-//int main()
-//{
-//	setlocale(0, "");
-//	int dM, dW;
-//	cout << "Введите дату: ";
-//	cout << "День месяца: ";
-//	cin >> dM;
-//	cout << "Номер дня в неделе: ";
-//	cin >> dW;//
-//	if ((13 == dM) && (5 == dW || 2 == dW) || (dM == 17 && dW == 5))
-//		cout << "неудачный день";//	return 0;//}
+//
+//int main(){
+//    setlocale(0, "");
+//    int n;
+//    string ending;
+//    cout << "Введите количество грибов: " << endl;
+//    cin >> n;
+//
+//    if (n % 100 >= 11 && n % 100 <= 19) ending = "ов";
+//    else switch (n % 10) {
+//    case 1: ending = ""; break;
+//    case 2:
+//    case 3:
+//    case 4: ending = "а"; break;
+//    default: ending = "ов"; break;
+//    }
+//    cout << "Джонни собрал " << n << " гриб" << ending << endl;
+//    return 0;
+//}
+
+
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <limits>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    double x;
+    cout << "Введите значение х: " << endl; cin >> x;
+
+    if (x >= -2.5 && x <= 2.5) {
+        cout << "r = " << (5.0 / 2.0) * x * x * x * x;
+    }
+    else if (x > 2.5) {
+        cout << "r = " << sqrt(2.0 * x + 1.0);
+    }
+    else {
+        double denominator = x - 3.0;
+        if (abs(denominator) < numeric_limits<double>::epsilon()) {
+            cout << "Ошибка: деление на ноль";
+        }
+        else {
+            cout << "r = " << (sin(M_PI * abs(x))) / denominator;
+        }
+    }
+    return 0;
+}
