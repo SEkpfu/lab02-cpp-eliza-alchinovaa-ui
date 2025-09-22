04c6b1247944f6ce3f039dd075935e1e7639200b
eliza-alchinovaa-ui <eliza-alchinovaa@mail.ru>
2025-09-22 21:24:46 +0300

    Add files via upload

---
+++ task2.individ.cpp
@@ -0,0 +1,26 @@
+//#define _USE_MATH_DEFINES
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//int main() {
+//	setlocale(0, "");
+//	double x;
+//    
+//	cout << "¬ведите значение х: " << endl; cin >> x;
+//    double n = M_PI * abs(x);
+//    if (x >= -2.5 && x <= 2.5) {
+//        cout << "r = " << (5.0 / 2.0) * x * x * x * x;
+//    }
+//    else if (x > 2.5) {
+//        cout << "r = " << sqrt(2.0 * x + 1.0);
+//    }
+//    else {
+//        if (x == 3.0) {
+//            cout << "ќшибка: деление на ноль при x = 3.0";
+//        }
+//        else {
+//            cout << "r = " << (sin (n) ) / (x - 3.0);
+//        }
+//    }
+//	return 0;
+//}
---
+++ task4.individ.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main(){
+//    setlocale(0, "");
+//    int n;
+//    string ending;
+//    cout << "Введите количество грибов: " << endl;
+//    cin >> n;
+//
+//    if (n % 100 >= 11 && n % 100 <= 19) ending = "ов";
+//    else switch (n % 10) {
+//    case 1: ending = ""; break;
+//    case 2:
+//    case 3:
+//    case 4: ending = "а"; break;
+//    default: ending = "ов"; break;
+//    }
+//    cout << "Джонни собрал " << n << " гриб" << ending << endl;
+//    return 0;
+//}
